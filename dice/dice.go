9d5d3ca2d51cc6fc406524168514b321ee7d0660
Andy Yates <ayates@hpe.com>
2021-09-01 16:51:02 -0500

    Update dice.go

--- dice/dice.go
+++ dice/dice.go
@@ -7,10 +7,20 @@ import (
 )
 
 func main() {
-	d1 := die.NewDie(6)
-	d2 := die.NewDie(6)
+	var mydice [2]die.Die
 
-	d1.Roll()
-	d2.Roll()
-	fmt.Println("Dice", d1.Value, d2.Value)
+	for i := range mydice {
+		mydice[i] = *die.NewDie(6)
+	}
+
+	for i := 5; i > 0; i-- {
+		fmt.Print("Dice ", mydice[0].Roll(), mydice[1].Roll())
+		if mydice[0].Value == 1 && mydice[1].Value == 1 {
+			fmt.Print(" Snake Eyes!")
+		} else if mydice[0].Value == mydice[1].Value {
+			fmt.Print(" Doubles!")
+		}
+		fmt.Print("\n")
+	}
+
 }
